8c16769133f5b167accefd9a11f139ca2b31641d
Vitaly Chipounov <vitaly.chipounov@epfl.ch>
2011-05-16 00:12:13 +0200

    Initial web site with icon, logo, schema

---
+++ www/index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<title>MagicTunnel</title>
+<style type="text/css">
+  @import url(/stylesheet.css);
+</style>
+</head>
+
+<body>
+<div id="header">
+<div id="header-container">
+<ul>
+<li class="selected"><a href="/index.php"><span>Home</span></a></li>
+<li><a href="download.php"><span>Download</span></a></li>
+<li><a href="faq.php"><span>FAQ</span></a></li>
+</ul>
+</div>
+</div>
+
+<div id="content">
+   <div id="content-container">
+     <div id="schema">
+       <div id="schema-1-pda">
+         <h3>Android Smartphone</h3>
+         Wraps Internet traffic into DNS queries with the MagicTunnel client
+       </div>
+       <div id="schema-2-ispserver">
+         <h3>Network Provider</h3>
+         Blocks all communications except DNS queries
+       </div>
+       <div id="schema-3-server">
+         <h3>DNS Server</h3>
+   Extracts the data from the DNS queries and forwards them to the Internet
+       </div>
+     </div>
+   </div>
+</div>
+
+</body>
+
+</html>
