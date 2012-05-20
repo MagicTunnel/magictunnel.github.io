42ff9e72d8e69fcd7dbe46a5cf8dde642f46b730
vitaly <vitaly@grml.(none)>
2012-05-20 15:23:47 +0000

    Added flattr donation

--- www/donate.php
+++ www/donate.php
@@ -4,6 +4,19 @@
 <style type="text/css">
   @import url(/stylesheet.css);
 </style>
+
+<script type="text/javascript">
+/* <![CDATA[ */
+    (function() {
+        var s = document.createElement('script'), t = 
+document.getElementsByTagName('script')[0];
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
+        t.parentNode.insertBefore(s, t);
+    })();
+/* ]]> */</script>
+
 </head>
 
 <body>
@@ -29,6 +42,17 @@ Find MagicTunnel useful? Donate bitcoins to:
 <p align="center">
 Check out <a href="http://www.bitcoin.org">http://www.bitcoin.org</a> to learn how to obtain and use bitcoins, a peer-to-peer, easy-to-use virtual currency.
 </p>
+</div>
+<div>
+<p align="center">
+
+<a class="FlattrButton" style="display:none;" rev="flattr;button:compact;" 
+href="http://www.magictunnel.net/"></a>
+<noscript><a href="http://flattr.com/thing/688067/MagicTunnel" target="_blank">
+<img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" 
+title="Flattr this" border="0" /></a></noscript>
+</p>
+
 </div>
 </div>
   </div>
